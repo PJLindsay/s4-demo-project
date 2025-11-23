bb83134b982240f7c5a2ec63449b69acd553c507
PJ Lindsay <pjlindsay@gmail.com>
2025-11-23 16:51:40 -0700

    Expose component APIs with useImperativeHandle

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,38 @@
-import type { FormEvent, ComponentPropsWithoutRef } from 'react'
+import {
+  type FormEvent,
+  type ComponentPropsWithoutRef,
+  useRef,
+  useImperativeHandle,
+  forwardRef,
+} from 'react'
+
+// should describe object returned from useImperativeHandle
+// (the value of the ref here)
+export type FormHandle = {
+  clear: () => void
+}
 
 type FormProps = ComponentPropsWithoutRef<'form'> & {
   onSave: (value: unknown) => void
 }
 
-export default function Form({ onSave, children, ...otherProps }: FormProps) {
+const Form = forwardRef<FormHandle, FormProps>(function Form(
+  { onSave, children, ...otherProps },
+  ref
+) {
+  const form = useRef<HTMLFormElement>(null)
+
+  // used if you want to expose callable functions to another component
+  // only works in a component that receives forwardedRef
+  useImperativeHandle(ref, () => {
+    return {
+      clear() {
+        console.log('CLEARING')
+        form.current?.reset()
+      },
+    }
+  })
+
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
 
@@ -14,10 +42,14 @@ export default function Form({ onSave, children, ...otherProps }: FormProps) {
     const data = Object.fromEntries(formData)
     // pass data to form that's handling the save
     onSave(data)
+
+    form.current?.reset()
   }
   return (
-    <form onSubmit={handleSubmit} {...otherProps}>
+    <form onSubmit={handleSubmit} {...otherProps} ref={form}>
       {children}
     </form>
   )
-}
+})
+
+export default Form
