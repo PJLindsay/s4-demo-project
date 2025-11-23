724d1c49d9e39aa02829f3554f1160f3404bf9ec
PJ Lindsay <pjlindsay@gmail.com>
2025-11-23 16:21:53 -0700

    Add basic Form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { useRef } from 'react'
 import Input from './components/Input.tsx'
+import Form from './components/Form.tsx'
 
 function App() {
-  const input = useRef<HTMLInputElement>(null)
   return (
     <main>
-      <Input label="Test" id="test" ref={input} />
+      <Form>
+        <Input type="text" label="Name" id="name" />
+        <Input type="number" label="Age" id="age" />
+      </Form>
     </main>
   )
 }
