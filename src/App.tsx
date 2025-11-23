44b1f94b9c0d97f4c2723e99ddd0fdcd45991f6a
PJ Lindsay <pjlindsay@gmail.com>
2025-11-23 16:35:45 -0700

    Sharing logic with unknown and Type Casting

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import { useRef } from 'react'
 import Input from './components/Input.tsx'
 import Form from './components/Form.tsx'
+import Button from './components/Button.tsx'
 
 function App() {
+  function handleSave(data: unknown) {
+    const extractedData = data as { name: string; age: string }
+    console.log(extractedData)
+  }
+
   return (
     <main>
-      <Form>
+      <Form onSave={handleSave}>
         <Input type="text" label="Name" id="name" />
         <Input type="number" label="Age" id="age" />
+        <p>
+          <Button>Save</Button>
+        </p>
       </Form>
     </main>
   )
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,23 @@
-import { ComponentPropsWithoutRef } from 'react'
+import type { FormEvent, ComponentPropsWithoutRef } from 'react'
 
-type FormProps = ComponentPropsWithoutRef<'form'>
+type FormProps = ComponentPropsWithoutRef<'form'> & {
+  onSave: (value: unknown) => void
+}
 
-export default function Form(props: FormProps) {
-  return <form {...props}>{props.children}</form>
+export default function Form({ onSave, children, ...otherProps }: FormProps) {
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    // get all values entered into any input field (as long as it has a name property)
+    const formData = new FormData(event.currentTarget)
+    // convert formData to a simpler object
+    const data = Object.fromEntries(formData)
+    // pass data to form that's handling the save
+    onSave(data)
+  }
+  return (
+    <form onSubmit={handleSubmit} {...otherProps}>
+      {children}
+    </form>
+  )
 }
