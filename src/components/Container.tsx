cf4d0d97c660d154a38b0426ad9cc55f3368a4e4
PJ Lindsay <pjlindsay@gmail.com>
2025-11-23 15:46:22 -0700

    Enhance Polymorphic Component with Generics

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,14 +1,22 @@
-import { ElementType } from 'react'
+import {
+  type ComponentPropsWithoutRef,
+  type ElementType,
+  type ReactNode,
+} from 'react'
 
-// A polymorphic wrapper component
-// won't know which component it will wrap
+// A polymorphic wrapper component - won't know which component it will wrap
 
 // as receives the identifier of the component that should be returned
-type ContainerProps = {
-  as: ElementType
-}
+type ContainerProps<T extends ElementType> = {
+  as?: T
+  children: ReactNode
+} & ComponentPropsWithoutRef<T>
 
-export default function Container({ as }: ContainerProps) {
-  const Component = as
-  return <Component />
+export default function Container<C extends ElementType>({
+  as,
+  children,
+  ...props
+}: ContainerProps<C>) {
+  const Component = as || 'div'
+  return <Component {...props}>{children}</Component>
 }
