f7cdcb88e59a8b99ae0f38e0437cfc394f33c00d
PJ Lindsay <pjlindsay@gmail.com>
2025-11-23 16:53:04 -0700

    Use Type Guards to narrow down type so TS can infer final type

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import { useRef } from 'react'
 
 import Input from './components/Input.tsx'
-import Form, {type FormHandle} from './components/Form.tsx'
+import Form, { type FormHandle } from './components/Form.tsx'
 import Button from './components/Button.tsx'
 
 function App() {
-
   const customForm = useRef<FormHandle>(null)
 
   function handleSave(data: unknown) {
-    const extractedData = data as { name: string; age: string }
-    console.log(extractedData)
+    // const extractedData = data as { name: string; age: string };
+    if (
+      !data ||
+      typeof data !== 'object' ||
+      !('name' in data) ||
+      !('age' in data)
+    ) {
+      return
+    }
+
+    // at this point, TypeScript knows that data MUST BE an object
+    // with a name and age property
+    // otherwise, the previous if statement would have returned
+    console.log(data)
     customForm.current?.clear()
   }
 
